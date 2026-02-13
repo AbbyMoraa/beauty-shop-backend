aae6fddc829d62d2bf80d5770d85708199204650
Manka.code <Abdirahman.wsh@gmail.com>
2026-02-13 09:55:58 +0300

    rename app.py to application.py to fix deployment

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__))
 
-import app as app_module
-app = app_module.app
+import application
+app = application.app
 
 if __name__ == "__main__":
     app.run()
