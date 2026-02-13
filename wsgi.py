e98115afddd2bf19a41cc30a79612a80c393c6e6
Manka.code <Abdirahman.wsh@gmail.com>
2026-02-13 09:33:00 +0300

    fix wsgi import for deployment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from app import app
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
+import app as app_module
+app = app_module.app
 
 if __name__ == "__main__":
     app.run()
